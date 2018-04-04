0e8b2a1cb2c2ab83133c3c9ac821a136b3b4a546
MatteolusBucket <matteo.maso.1@studenti.unipd.it>
2018-04-04 15:56:00 +0200

    made compilable from clion

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 // Created by matte on 3/31/2018.
 //
 
+#include <sdsl/suffix_trees.hpp>
 #include <iostream>
+#include <string>
 
-int main()
+using namespace std;
+using namespace sdsl;
+
+typedef cst_sct3<> cst_t;
+
+int main(int argc, char* argv[])
 {
-    printf("Hello, World!");
-    return 0;
+    if (argc < 2) {
+        cout << "usage: "<<argv[0]<< " file" << std::endl;
+        return 1;
+    }
+
+    cst_t cst;
+    construct(cst, argv[1], 1);
+
+    typedef cst_bfs_iterator<cst_t> iterator;
+    iterator begin = iterator(&cst, cst.root());
+    iterator end   = iterator(&cst, cst.root(), true, true);
+
+    for (iterator it = begin; it != end; ++it) {
+        std::cout << cst.depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << std::endl;
+    }
+
 }
