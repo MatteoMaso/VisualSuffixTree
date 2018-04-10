c0eeeadc84afaa374d41f37a319a3abaeb03109f
MatteolusBucket <matteo.maso.1@studenti.unipd.it>
2018-04-10 10:55:01 +0200

    Add edge representation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-//
-// Created by matte on 3/31/2018.
-//
-
 #include <sdsl/suffix_trees.hpp>
 #include <iostream>
 #include <string>
@@ -18,15 +14,52 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    cst_t cst;
-    construct(cst, argv[1], 1);
+    cst_t cst;                      //declare the suffix tree
+    construct(cst, argv[1], 1);     //initialize the suffix tree
 
     typedef cst_bfs_iterator<cst_t> iterator;
     iterator begin = iterator(&cst, cst.root());
     iterator end   = iterator(&cst, cst.root(), true, true);
 
+    int str_length = 0;
+
     for (iterator it = begin; it != end; ++it) {
-        std::cout << cst.depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << std::endl;
+//        std::cout << " ID: " << cst.id(*it) << " DEPTH: " << cst.depth(*it) << " NODEDEPTH: " << cst.node_depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << std::endl;
+
+        str_length = cst.depth(*it);
+
+        string edge = "";
+
+        if( (cst.node_depth(*it) == 0)||(cst.node_depth(*it) == 1 && str_length == 1 && (cst.lb(*it) == cst.rb(*it))) ){
+            edge = "-";
+        } else {
+
+            if (cst.lb(*it) == cst.rb(*it)) {
+
+                edge = "leaft:         ";
+
+                for (int i = 1; i < str_length - 1; i++) {
+                    edge += cst.edge(*it, i);
+                }
+
+                edge += "#";
+                edge += "#";
+
+            } else {
+
+                edge = "internal_node: ";
+
+                for (int i = 1; i <= str_length; i++) {
+                    edge += cst.edge(*it, i);
+                }
+            }
+
+
+
+
+        }
+
+        std::cout << "NodeDepth: " << cst.node_depth(*it) << " Depth: " << cst.depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << edge << std::endl;
     }
 
 }
