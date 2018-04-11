25801620f921256475f2dfbab5c8cd1dd8dca678
MatteolusBucket <matteo.maso.1@studenti.unipd.it>
2018-04-11 11:55:27 +0200

    Separate the two programs

--- main.cpp
+++ main.cpp
@@ -7,15 +7,58 @@ using namespace sdsl;
 
 typedef cst_sct3<> cst_t;
 
+/**
+ *
+ * @param inputFileName file that contains the strings that you want to analyze
+ */
+void treeParser(char* inputFileName);
+
+/**
+ * This method take an input file well formatted and create a flameGraph as output
+ * @param inputFileName formatted file to convert in FlameGraph SVG
+ */
+void createSvg(char* inputFileName);
+
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
-        cout << "usage: "<<argv[0]<< " file" << std::endl;
+        cout << "insert -data nameInput for starting treeParser or -svg to create the SVG file" << std::endl;
         return 1;
     }
 
-    cst_t cst;                      //declare the suffix tree
-    construct(cst, argv[1], 1);     //initialize the suffix tree
+    char firstProgram[6]  = "-data";
+    char secondProgram[5] = "-svg";
+
+    if (strcmp(argv[1], firstProgram) == 0){
+        treeParser(argv[2]);
+    } else if (strcmp(argv[1], secondProgram) == 0){
+        createSvg(argv[2]);
+    } else {
+        printf("Bad arguments!");
+    }
+}
+
+
+void createSvg(char* inputFileName){
+    //BEGIN SVG CREATOR
+//    todo to implement
+};
+
+/**
+ * FORMAT OUTPUT FILE
+ * ogni parametro è separato da ":"
+ *
+ * PARAM1: node_depth
+ * PARAM2: depth
+ * PARAM3:
+ * PARAM4:
+ */
+void treeParser(char* inputFileName) {
+
+    //BEGIN TREE PARSER
+
+    cst_t cst;                              //declare the suffix tree
+    construct(cst, inputFileName, 1);       //initialize the suffix tree
 
     typedef cst_bfs_iterator<cst_t> iterator;
     iterator begin = iterator(&cst, cst.root());
@@ -24,7 +67,6 @@ int main(int argc, char* argv[])
     int str_length = 0;
 
     for (iterator it = begin; it != end; ++it) {
-//        std::cout << " ID: " << cst.id(*it) << " DEPTH: " << cst.depth(*it) << " NODEDEPTH: " << cst.node_depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << std::endl;
 
         str_length = cst.depth(*it);
 
@@ -53,13 +95,8 @@ int main(int argc, char* argv[])
                     edge += cst.edge(*it, i);
                 }
             }
-
-
-
-
         }
 
         std::cout << "NodeDepth: " << cst.node_depth(*it) << " Depth: " << cst.depth(*it) << "-[" << cst.lb(*it) << "," << cst.rb(*it) << "]" << edge << std::endl;
     }
-
-}
+};
