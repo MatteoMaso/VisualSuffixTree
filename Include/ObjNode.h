ca351caed983e0bdbe9a92ca5005c4faa302c5e4
Alessia Perissinotto <alessia.perissinotto@studenti.unipd.it>
2018-05-07 08:33:13 -0700

    Updated 23/04/2018

---
+++ Include/ObjNode.h
@@ -0,0 +1,26 @@
+//
+// Created by alessia on 07/05/18.
+//
+#include <string>
+#include "NodeInfoStructure.h"
+
+#ifndef VISUALSUFFIXTREE_OBJNODE_H
+#define VISUALSUFFIXTREE_OBJNODE_H
+
+using namespace std;
+
+class ObjNode{
+
+public:
+    ObjNode();
+
+
+    int objNodeDepth;
+    int objNodeX;
+    int objNodeY;
+    int objNodeWid;
+    //int ovjNodeHeigth; per ora li facciamo tutti alla stessa altezza
+
+
+};
+#endif //VISUALSUFFIXTREE_OBJNODE_H
